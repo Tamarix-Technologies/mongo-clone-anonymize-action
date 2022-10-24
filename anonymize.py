b1a7994aece4b14f53fba462064f06444a5d1eed
davideferri <davide.ferri.94@gmail.com>
2022-10-24 14:16:10 +0100

    Added test script to anonymize data

--- anonymize.py
+++ anonymize.py
@@ -8,9 +8,6 @@ db = get_db(db_url)
 
 # pipeline steps
 stage_1 = {'username': {'$function': {'lang': 'js', 'args': ['$_id'], 'body': hashlib.md5}}}
-pipeline = [{'$set': stage_1}]
-pipeline.push(
-    {'$merge': {'into': { 'db': 'testdata', 'coll': 'payments'}, 'on': '_id',  'whenMatched': 'replace', 'whenNotMatched': 'fail'}}
-)
+pipeline = [{'$set': stage_1},{'$merge': {'into': { 'db': 'appdb', 'coll': 'users'}, 'on': '_id',  'whenMatched': 'replace', 'whenNotMatched': 'fail'}}]
 db.users.aggregate(pipeline)
 db.users.find()
