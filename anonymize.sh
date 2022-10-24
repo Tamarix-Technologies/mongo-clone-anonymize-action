e49a80700be96746d556359a21d7e4ad4463dd4d
davideferri <davide.ferri.94@gmail.com>
2022-10-24 12:32:44 +0100

    Added test script to anonymize data

--- anonymize.sh
+++ anonymize.sh
@@ -5,4 +5,4 @@ MONGO_URI_TARGET=$2
 DB_NAME_SOURCE="${MONGO_URI_SOURCE##*/}"
 DB_NAME_TARGET="${MONGO_URI_TARGET##*/}"
 
-python ./anonymize.py MONGO_URI_TARGET
+python anonymize.py MONGO_URI_TARGET
