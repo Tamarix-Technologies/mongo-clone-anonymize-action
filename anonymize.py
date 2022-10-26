3e82b282a174ee5c2ef0f6ac27cc61cd8823375a
davideferri <davide.ferri.94@gmail.com>
2022-10-26 13:28:57 +0100

    Send mail with preprod details [MVP-207]

--- anonymize.py
+++ anonymize.py
@@ -11,29 +11,34 @@ db = get_db(db_url)
 
 # anonymize users data
 users = db['users'].find({})
-user_map = {}
+username_map = {}
+password_map = {}
+letters = string.ascii_lowercase
 for user in users:
     username = user['username']
-    if username in user_map:
-        user['username'] = user_map[username]
+    if username in username_map:
+        user['username'] = username_map[username]
+        user['hashed_password'] = get_password_hash(password_map[username])
     else:
-        user_map[username] = str(random.randint(0,10000000000))
-        user['username'] = user_map[username]
-    user['hashed_password'] = get_password_hash('ciao')
+        username_map[username] = str(random.randint(0,10000000000))
+        password_map[username] = ''.join(random.choice(letters) for i in range(10))
+        user['username'] = username_map[username]
+        user['hashed_password'] = get_password_hash(password_map[username])
     db['users'].replace_one({'_id':user['_id']}, user, upsert=False)
 
 # anonymize portfolios
 portfolios = db['portfolios'].find({})
 port_map = {}
-letters = string.ascii_lowercase
+i = 0
 for portfolio in portfolios:
-    portfolio['username'] = user_map[portfolio['username']]
+    portfolio['username'] = username_map[portfolio['username']]
     port_name = portfolio['port_name']
     if port_name in port_map:
         portfolio['port_name'] = port_map[portfolio['port_name']]
     else:
-        port_map[port_name] = ''.join(random.choice(letters) for i in range(20))
+        port_map[port_name] = f'Portfolio {i}'
         portfolio['port_name'] = port_map[port_name]
+        i += 1
 
     portfolio['port_data']['fund_name'] = [str(i) for i in range(len(portfolio['port_data']['fund_name']))]
     db['portfolios'].replace_one({'_id':portfolio['_id']}, portfolio, upsert=False)
@@ -41,17 +46,17 @@ for portfolio in portfolios:
 # anonymize figures
 figures = db['figures'].find({})
 for figure in figures:
-    figure['username'] = user_map[figure['username']]
+    figure['username'] = username_map[figure['username']]
     figure['port_name'] = port_map[figure['port_name']]
     for subfigure in figure['figures']:
-        subfigure['username'] = user_map[subfigure['username']]
+        subfigure['username'] = username_map[subfigure['username']]
         subfigure['port_name'] = port_map[subfigure['port_name']]
     db['figures'].replace_one({'_id':figure['_id']}, figure, upsert=False)
 
 # anonymize scenarios
 scenarios = db['scenarios'].find({})
 for scenario in scenarios:
-    scenario['username'] = user_map[scenario['username']]
+    scenario['username'] = username_map[scenario['username']]
     scenario['port_name'] = port_map[scenario['port_name']]
     if 'overrides' in scenario['scenario_data']:
         scenario['scenario_data']['overrides'] = {}
@@ -62,19 +67,20 @@ for scenario in scenarios:
 # anonymize targets
 targets = db['targets'].find({})
 for target in targets:
-    target['username'] = user_map[target['username']]
+    target['username'] = username_map[target['username']]
     target['port_name'] = port_map[target['port_name']]
     db['targets'].replace_one({'_id':target['_id']}, target, upsert=False)
 
 # anonymize roadmaps
 roadmaps = db['roadmaps'].find({})
 for roadmap in roadmaps:
-    roadmap['username'] = user_map[roadmap['username']]
+    roadmap['username'] = username_map[roadmap['username']]
     roadmap['port_name'] = port_map[roadmap['port_name']]
     db['roadmaps'].replace_one({'_id':roadmap['_id']}, roadmap, upsert=False)
 
+out = {'username':username_map,'password':password_map}
 with open('./user_map.json', 'w') as f:
-    json.dump(user_map, f)
+    json.dump(out, f)
 
 
 
